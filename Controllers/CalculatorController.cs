080aadef04cd7580aa2ec6b3db2cc37e55def36c
speehive <speehive@DESKTOP-TPF0JOE>
2022-12-22 21:05:44 +0530

    Add project files.

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWebsite.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        
+        public IActionResult Index(string firstNum, string secondNum, string Cal)
+        {
+            int a = Convert.ToInt32(firstNum);
+            int b = Convert.ToInt32(secondNum);
+            int c = 0;
+            switch (Cal)
+            {
+                case "Add":
+                    c = a + b;
+                    break;
+                case "Sub":
+                    c = a - b;
+                    break;
+                case "Mul":
+                    c = a * b;
+                    break;
+                case "Div":
+                    c = a / b;
+                    break;
+            }
+            ViewBag.Result = c;
+            return View();
+
+        }
+       
+    }
+}
---
+++ Models/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace CalculatorWebsite.Models
+{
+    public class Calculator
+    {
+        public int add { get; set; }
+
+        public int sub { get; set; }
+
+        public int mul { get; set; }
+
+        public int div { get; set; }
+    }
+}
